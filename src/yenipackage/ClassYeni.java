02f7ac8a3c11d992e5ce96f7ad88e6b4e89584d2
Erkan <furkan29orhan@gmail.com>
2020-07-05 22:16:35 +0200

    mesaj

--- src/yenipackage/ClassYeni.java
+++ src/yenipackage/ClassYeni.java
@@ -4,6 +4,8 @@ public class ClassYeni {
 
 	public static void main(String[] args) {
 		System.out.println("ilk commit masterdan");
-	}
 
+		System.out.println("mrb hocam , lutfen daha cok tekrar yapalim");
+
+}
 }
